53e1a8ecfad905592594b307daa96a223f634602
MerlinaDS <merlina@aguel.com.ar>
2024-12-23 15:06:31 -0300

    Carga de archivos al repositorio

--- conecta.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-def conexion():
-    return pymysql.connect(
-        host='aguel.com.ar',
-        user='aguelca_aguelca',
-        password='gdaU0908',
-        database='aguelca_bdd'
-    )
